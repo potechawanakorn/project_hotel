5814731454ad32a5ca5d9c85e09edb60da2de157
potechawanakorn <120349845+potechawanakorn@users.noreply.github.com>
2023-02-12 19:38:35 +0700

    modal dan proses hapus

---
+++ app/Http/Controllers/BookingController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class BookingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $booking = new Booking;
+
+        $booking->kode = $request->kode;
+        $booking->booking = $request->booking;
+        $booking->save();
+
+        return redirect('/booking');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $booking = Booking::find($id);
+        return view('booking.edit',compact('booking'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $booking = new Booking;
+
+        $booking->kode = $request->kode;
+        $booking->booking = $request->booking;
+        $booking->save();
+
+        return redirect('/booking');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $booking = Booking::find($id);
+        $booking->delete();
+
+        return redirect('/booking');
+    }
+}
