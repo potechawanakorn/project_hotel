ca499da29e6c9e9f95a2211a1c170f12df169ae6
potechawanakorn <120349845+potechawanakorn@users.noreply.github.com>
2023-02-12 20:15:23 +0700

    tambah tamu

---
+++ resources/views/Tamu/form.blade.php
@@ -0,0 +1,75 @@
+@extends('layouts.master')
+@section('judul','Tambah Data tamu')
+@section('content-header')
+<section class="content-header">
+    <div class="container-fluid">
+      <div class="row mb-2">
+        <div class="col-sm-6">
+          <h1>Tambah Data tamu</h1>
+        </div>
+        <div class="col-sm-6">
+          <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item active">Tambah Data tamu</li>
+          </ol>
+        </div>
+      </div>
+    </div><!-- /.container-fluid -->
+  </section>
+@endsection
+
+@section('content')
+<section class="content">
+
+    <!-- Default box -->
+    <div class="card">
+      <div class="card-header">
+        
+
+        <div class="card-tools">
+          <button type="button" class="btn btn-tool" data-card-widget="collapse" title="Collapse">
+            <i class="fas fa-minus"></i>
+          </button>
+          <button type="button" class="btn btn-tool" data-card-widget="remove" title="Remove">
+            <i class="fas fa-times"></i>
+          </button>
+        </div>
+      </div>
+      <div class="card-body">
+        <form method="POST" action="/tamu">
+            @csrf
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Nomor Kamar</label>
+                <input type="text" value="{{$tamu->nomor}}" name="tempat" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+              </div>
+
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Jenis Kamar</label>
+              <input type="text" name="jenis" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">Jumlah</label>
+              <input type="date" name="jumlah" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+
+            <div class="mb-3">
+              <label for="exampleInputEmail1" class="form-label">booking</label>
+              <select name="booking" class="form-control" id="">
+                <option value="">-Pilih kamar-</option>
+                @foreach($booking as $data)
+                <option value="{{$data->id}}">{{$data->kode}} - {{$data->booking}}</option>
+                @endforeach
+              </select>
+            </div>
+            
+            <button type="submit" class="btn btn-primary">Tambah Data</button>
+          </form>
+      </div>
+      <!-- /.card-body -->
+      
+    </div>
+    <!-- /.card -->
+
+  </section>
+@endsection
